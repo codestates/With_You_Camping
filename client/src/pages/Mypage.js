835cd60ca954d3ed588bf4ca47ca88d0d2a657be
happy8131 <lion8131@naver.com>
2022-04-25 08:29:15 +0900

    login signup modal

--- client/src/pages/Mypage.js
+++ client/src/pages/Mypage.js
@@ -1,17 +1,8 @@
 import React from "react";
-import { NavLink } from "react-router-dom";
-import styled from "styled-components";
-
-const Div = styled.div`
-  margin: 50px 50px;
-`;
 
 function Mypage() {
   return (
     <>
-      <Div>
-        <NavLink to="/">Home</NavLink>
-      </Div>
       <div>Mypage</div>
     </>
   );
---
+++ client/src/pages/Postlist.js
@@ -0,0 +1,15 @@
+import React from "react";
+import { NavLink } from "react-router-dom";
+
+function Postlist() {
+  return (
+    <>
+      <div>게시글 목록 페이지</div>
+      <div>
+        <NavLink to="/postdetail">게시글 상세페이지</NavLink>
+      </div>
+    </>
+  );
+}
+
+export default Postlist;
